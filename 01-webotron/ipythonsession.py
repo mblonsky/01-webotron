316a91b13a29021a9cab6464f6ca11b93fd1ac28
mblonsky <mblonsky@orion.mikeblonsky.com>
2019-09-13 13:43:28 -0700

    webotron project

---
+++ 01-webotron/ipythonsession.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket)
+   
+ec2_client = session.client('ec2') 
