fb9b39ce0d075e13414f344069cc9ddac0397956
mblonsky <mblonsky@orion.mikeblonsky.com>
2019-11-13 17:28:24 -0800

    Allow user to set AWS profile

---
+++ 01-webotron/README.md
@@ -0,0 +1,20 @@
+#Automating AWS with Python
+
+Repository for the A Cloud Guru course *Automating AWS with Python*
+
+## 01-webotron
+
+Webotron is a script that will sync a local directory to an s3 bucket, and optionally configure Route 53 and cloudfront as well.
+
+
+### Features
+
+Webotron currently has the following features:
+
+- List bucket
+- List contents of a bucket
+- Create and set up bucket
+- Sync directory tree to bucket
+- Set AWS profile with --profile=<profileName>
+
+
--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -18,13 +18,20 @@ import click
 
 from bucket import BucketManager
 
-session = boto3.Session(profile_name='pythonAutomation')
-bucket_manager = BucketManager(session)
-
+session = None
+bucket_manager = None
 
 @click.group()
-def cli():
+@click.option('--profile', default=None,
+    help="Use a given AWS profile.")
+def cli(profile):
     """Webotron deploys websites to AWS."""
+    global session, bucket_manager
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+    session = boto3.Session(**session_cfg)
+    bucket_manager = BucketManager(session)
     pass
 
 
