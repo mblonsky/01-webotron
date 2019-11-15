83a0b22aa169097f47e24c28ecf8ba1849e20502
mblonsky <mblonsky@orion.mikeblonsky.com>
2019-11-14 16:17:10 -0800

    Output website host when syncing

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -21,9 +21,10 @@ from bucket import BucketManager
 session = None
 bucket_manager = None
 
+
 @click.group()
 @click.option('--profile', default=None,
-    help="Use a given AWS profile.")
+              help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS."""
     global session, bucket_manager
@@ -67,6 +68,7 @@ def setup_bucket(bucket):
 def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET."""
     bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
 
 
 if __name__ == '__main__':
---
+++ lint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+/home/mblonsky/.local/share/virtualenvs/mblonsky-MKdnuDBc/bin/pycodestyle 01-webotron/webotron/
+/home/mblonsky/.local/share/virtualenvs/python-Tjn8fF8-/bin/pydocstyle 01-webotron/webotron/
+/home/mblonsky/.local/share/virtualenvs/01-webotron-WNciPVJk/bin/pyflakes 01-webotron/webotron/
