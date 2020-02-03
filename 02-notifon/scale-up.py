2a24e98971044acc03f1f4ee05e86ae34f4ab3ed
mblonsky <mblonsky@orion.mikeblonsky.com>
2020-02-02 20:20:56 -0800

    adding scale-up.py for triggering an auto-scaling scale up event on a specific auto-scaling group

---
+++ 02-notifon/scale-up.py
@@ -0,0 +1,6 @@
+import boto3
+session = boto3.Session(profile_name='pythonAutomation')
+as_client = session.client('autoscaling')
+
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
+
